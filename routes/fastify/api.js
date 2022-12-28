79d8fdf4852a2bf721c375fc465f0ddbce98ebf3
thzero <thzero@users.noreply.github.com>
2022-12-27 18:40:36 -0600

    updates

--- routes/fastify/api.js
+++ routes/fastify/api.js
@@ -1,4 +1,5 @@
 import Constants from '../../constants.js';
+import LibraryConstants from '@thzero/library_server/constants.js';
 
 import BaseRoute from '@thzero/library_server_fastify/routes/index.js';
 
@@ -11,6 +12,7 @@ class ApiRoute extends BaseRoute {
 		await super.init(injector, app, config);
 		
 		this._inject(app, injector, Constants.InjectorKeys.SERVICE_API, Constants.InjectorKeys.SERVICE_API);
+		// this._inject(app, injector, LibraryConstants.InjectorKeys.SERVICE_UTILITY, LibraryConstants.InjectorKeys.SERVICE_UTILITY);
 	}
 
 	get id() {
@@ -18,6 +20,26 @@ class ApiRoute extends BaseRoute {
 	}
 
 	_initializeRoutes(router) {
+		router.post(this._join('/reset'),
+			// authentication(true),
+			// authorization('sync'),
+			{
+				preHandler: router.auth([
+					router.authenticationDefault,
+					router.authorizationDefault
+				], 
+				{ 
+					relation: 'and',
+					roles: [ 'sync' ]
+				}),
+			},
+			// eslint-disable-next-line
+			async (request, reply) => {
+				const response = (await router[LibraryConstants.InjectorKeys.SERVICE_UTILITY].syncFrom(request.correlationId)).check(request);
+				// https://github.com/fastify/fastify-compress/issues/215#issuecomment-1210598312
+				return this._jsonResponse(reply, response);
+			}
+		);
 	}
 }
 
